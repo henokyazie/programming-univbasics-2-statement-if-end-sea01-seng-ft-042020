565d71d2257b951febe7fa5b098109a6014de3c3
brave-ping-4201 <h_yazie@yahoo.com>
2020-04-01 04:42:12 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,29 @@
 # Write your solution here
+if TRUE
+  puts "Even!"
+else
+  puts "Odds!"
+end
+current_time = Time.now
+current_time = current_time.to_i
+current_time = Time.now
+current_time = current_time.to_i
+ 
+if current_time % 2 == 0
+  puts "Even!"
+else
+  puts "Odd!"
+end
+if Time.now.to_i % 2 == 0
+  puts "Even!"
+else
+  puts "Odd!"
+end
+current_time = Time.now
+current_time = current_time.to_i
+ 
+if current_time.even?
+  puts "Even!"
+else
+  puts "Odd!"
+end
